ebd581bc7c5756ef19351f9f0a029d5c5054e937
Ablimit Aji <aaji@mathcs.emory.edu>
2012-11-28 23:28:10 -0500

    LS problem generator

---
+++ hw4/genlsdata.m
@@ -0,0 +1,16 @@
+function [A b]= genlsdata(n)
+
+x = linspace(0,1,n+2);
+hh = 1/((n+1)*(n+1));
+
+% generate A1 for one-dimensional case.
+e =ones(n,1);
+
+A1 = spdiags([-e,2*e,-e],[-1,0,-1],n,n);
+A = kron(A1,speye(n)) + kron(speye(n),A1);
+
+% generate right values
+[X,Y] = meshgrid(x);
+Z = funf(X,Y); % evaluate functions 
+b = hh*Z(:); % reshape to get the f values
+
